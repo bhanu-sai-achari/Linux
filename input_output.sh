2ec8869b812dd2069b72736f0b685e9b17d9eec0
Bhanu-sai-achari <bhanu.rymec@gmail.com>
2022-09-07 10:50:34 +0530

    Reading input and displaying it

---
+++ input_output.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#author:Bhanu
+echo "Welcome to shell scripting learning sessions"
+echo 
+host=`hostname`
+echo "Hello, my name is $host, I'm your virtual assistant for registering Shell Scripting classes"
+echo 
+echo "Please signup for class"
+echo "Enter Your Full name"
+read name
+echo "enter your email"
+read email
+echo 
+echo hello $name , your $email id has been registered to online shell scripting class.
+echo "Thanks & Regards"
+echo "BSA"
