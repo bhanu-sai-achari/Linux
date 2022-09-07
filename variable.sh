41df76c52fd76916b89906dbf7cc739efeeaca36
Bhanu-sai-achari <bhanu.rymec@gmail.com>
2022-09-07 10:30:03 +0530

    variable practice

---
+++ variable.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#author Bhanu
+l=ls
+p=pwd
+wa=whoami
+
+echo "Variable practice"
+echo 
+echo $p
+echo
+echo $l
+echo
+echo $wa
+
+# using `` we can display commands
+echo 
+echo `$p`
+echo
+echo `$l`
+echo
+echo `$wa`
